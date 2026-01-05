09cff105a56b63d25fc1932fd66ed16c1c7f97c8
ukesh-01 <ukeshrv@gmail.com>
2026-01-05 11:31:18 +0530

    Fix DAG structure and add hello world DAG

---
+++ dags/example/hello_world_dag.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def say_hello():
+    print("Hello Airflow!")
+
+with DAG(
+    dag_id="hello_world_dag",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+
+    hello_task = PythonOperator(
+        task_id="say_hello",
+        python_callable=say_hello
+    )
