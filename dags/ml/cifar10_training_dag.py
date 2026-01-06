a389b86a8aae77565f6358cba25622bc7bf83447
ukesh-01 <ukeshrv@gmail.com>
2026-01-06 17:55:15 +0530

    dockeropertaor to kubernetes operator

--- dags/ml/cifar10_training_dag.py
+++ dags/ml/cifar10_training_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from datetime import datetime
 
 with DAG(
@@ -7,15 +7,16 @@ with DAG(
     start_date=datetime(2024, 1, 1),
     schedule=None,
     catchup=False,
-    tags=['ml', 'training'],
-)as dag:
+    tags=["ml", "training"],
+) as dag:
 
- train_model=DockerOperator(
-    task_id="cifar10_model",
-    image="cifar10-train:1.0",
-    api_version="auto",
-    auto_remove="success",
-    command="python train.py",
-    docker_url="unix://var/run/docker.sock",
-    network_mode="bridge"
-)
+    train_model = KubernetesPodOperator(
+        task_id="train_cifar10_model",
+        name="cifar10-train-pod",
+        namespace="airflow",
+        image="cifar10-train:1.0",
+        cmds=["python"],
+        arguments=["train.py"],
+        get_logs=True,
+        is_delete_operator_pod=True,
+    )
