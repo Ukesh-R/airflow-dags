a38acd20ca67b45555ddead3fa7a5d54179165dd
ukesh-01 <ukeshrv@gmail.com>
2026-01-06 16:11:42 +0530

    ML cifar dag file

---
+++ dags/ml/cifar10_training_dag.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="cifar10_training_dag",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=['ml', 'training'],
+)as dag:
+
+ train_model=DockerOperator(
+    task_id="cifar10_model",
+    image="cifar10-train:1.0",
+    api_version="auto",
+    auto_remove=True,
+    command="python train.py",
+    docker_url="unix://var/run/docker.sock",
+    network_mode="bridge"
+)
