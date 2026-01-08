d068a3f498ab4e3317c7485b17a2e4daabb39c7d
ukesh-01 <ukeshrv@gmail.com>
2026-01-08 09:32:34 +0530

    Initial commit

---
+++ dags/example/two_task_with_dependency.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def start():
+    print("task started")
+
+def end():
+    print("task ended")
+
+with dag(
+    dag_id="two_task_dependency_dag",
+    start_time=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+)as dag:
+
+    Start_task=PythonOperator(
+        task_id="start_task",
+        python_callable=start,
+    )
+
+    end_task=PythonOperator(
+        task_id="end_task",
+        python_callable=end,
+    )
+
+Start_task >> end_task
