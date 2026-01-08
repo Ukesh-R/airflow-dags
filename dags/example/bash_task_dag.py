eb2bbb5172bbba5e7008021e7c6bd484e5972e27
ukesh-01 <ukeshrv@gmail.com>
2026-01-08 10:47:32 +0530

    Initial commit

---
+++ dags/example/bash_task_dag.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+     dag_id="try_bash_dag",
+     start_date=datetime(2024, 1, 1),
+     schedule=None,
+     catchup=False,
+)as dag:
+    
+    bash_task=BashOperator(
+         task_id="bash_task",
+         bash_command="ecch 'Hello from Bash' && date && ls",
+    )
