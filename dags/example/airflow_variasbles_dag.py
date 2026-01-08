4a9a240e239e32135469bee745def619768c45b6
ukesh-01 <ukeshrv@gmail.com>
2026-01-08 12:17:02 +0530

    Initial Commit

---
+++ dags/example/airflow_variasbles_dag.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.models import Variable
+from datetime import datetime
+
+def variables_check():
+    env=Variables.get("env_name", default_var="local")
+    print(f"running in {env} environment")
+with DAG(
+    dag_id="airflow_variables",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+)as dag:
+    
+    PythonOperator(
+        task_id="airflow_variables_check",
+        python_callable=variables_check,
+    )
